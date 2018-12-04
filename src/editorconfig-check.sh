83e31c9f131464f519379480b534d25c9c1e94bc
Steimntz M. de Figueiredo <steimntz@tjro.jus.br>
2018-12-04 14:59:16 +0000

    validates bug https://gitlab.com/gitlab-org/gitlab-ce/issues/43365

--- src/editorconfig-check.sh
+++ src/editorconfig-check.sh
@@ -1,9 +1,18 @@
 #/bin/bash
+BASEDIR="$(dirname "$0")"
+BASENAME="$(basename "$0")"
+
 DIR=$1
 BEFORE_SHA=$2
 CURRENT_SHA=$3
 
 command="git --git-dir=$DIR/.git diff-tree --no-commit-id --name-only -r $BEFORE_SHA $CURRENT_SHA | xargs -i sh -c 'test -f {} && editorconfig-checker $DIR/{}'"
+
+if [[ "$#" -ne 3 || "$BEFORE_SHA" == "0000000000000000000000000000000000000000" ]]; then
+  echo "Usage: $BASEDIR/$BASENAME DIR BEFORE_COMMIT CURRENT_COMMIT"
+  exit 0
+fi
+
 
 if [ "$debug" == "on" ]; then
 	echo $command
--- tests/editorconfig-check.sh
+++ tests/editorconfig-check.sh
@@ -3,6 +3,18 @@
 BASEDIR="$(dirname "$0")"
 . $BASEDIR/assert.sh
 
-output="$(debug=on $BASEDIR/../src/editorconfig-check.sh ./windson A B)"
-expected="git --git-dir=./windson/.git diff-tree --no-commit-id --name-only -r A B | xargs -i sh -c 'test -f {} && editorconfig-checker ./windson/{}'"
+output="$(debug=on $BASEDIR/../src/editorconfig-check.sh ./windson AABA BBAB)"
+expected="git --git-dir=./windson/.git diff-tree --no-commit-id --name-only -r AABA BBAB | xargs -i sh -c 'test -f {} && editorconfig-checker ./windson/{}'"
 assertEquals "$output" "$expected"
+
+output="$(debug=on $BASEDIR/../src/editorconfig-check.sh ./windson A)"
+expected="Usage: tests/../src/editorconfig-check.sh DIR BEFORE_COMMIT CURRENT_COMMIT"
+assertEquals "$output" "$expected"
+
+output="$(debug=on $BASEDIR/../src/editorconfig-check.sh ./windson 0000000000000000000000000000000000000000 B)"
+expected="Usage: tests/../src/editorconfig-check.sh DIR BEFORE_COMMIT CURRENT_COMMIT"
+assertEquals "$output" "$expected"
+
+#output="$(debug=on $BASEDIR/../src/editorconfig-check.sh ./windson A 00000000000)"
+#expected="Usage: tests/../src/editorconfig-check.sh DIR BEFORE_COMMIT CURRENT_COMMIT"
+#assertEquals "$output" "$expected"
