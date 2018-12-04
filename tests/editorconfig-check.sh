1edcbea7ef04bcfe93782d781868a5b336aa1ecc
Steimntz M. de Figueiredo <steimntz@tjro.jus.br>
2018-12-04 15:23:55 +0000

    adds workspace dir and remove useless code

--- tests/editorconfig-check.sh
+++ tests/editorconfig-check.sh
@@ -4,7 +4,7 @@ BASEDIR="$(dirname "$0")"
 . $BASEDIR/assert.sh
 
 output="$(debug=on $BASEDIR/../src/editorconfig-check.sh ./windson AABA BBAB)"
-expected="git --git-dir=./windson/.git diff-tree --no-commit-id --name-only -r AABA BBAB | xargs -i sh -c 'test -f {} && editorconfig-checker ./windson/{}'"
+expected="git --git-dir=./windson/.git diff-tree --no-commit-id --name-only -r AABA BBAB | xargs -i sh -c 'cd ./windson && test -f {} && editorconfig-checker ./windson/{}'"
 assertEquals "$output" "$expected"
 
 output="$(debug=on $BASEDIR/../src/editorconfig-check.sh ./windson A)"
@@ -14,7 +14,3 @@ assertEquals "$output" "$expected"
 output="$(debug=on $BASEDIR/../src/editorconfig-check.sh ./windson 0000000000000000000000000000000000000000 B)"
 expected="Usage: tests/../src/editorconfig-check.sh DIR BEFORE_COMMIT CURRENT_COMMIT"
 assertEquals "$output" "$expected"
-
-#output="$(debug=on $BASEDIR/../src/editorconfig-check.sh ./windson A 00000000000)"
-#expected="Usage: tests/../src/editorconfig-check.sh DIR BEFORE_COMMIT CURRENT_COMMIT"
-#assertEquals "$output" "$expected"
