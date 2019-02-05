bfa1fa6eb0ce8f00ce9b67758612a336f16b4d7d
Steimntz M. de Figueiredo <steimntz@tjro.jus.br>
2019-02-05 14:29:14 +0000

    adjusts broken test

--- tests/editorconfig-check.tests.sh
+++ tests/editorconfig-check.tests.sh
@@ -4,7 +4,7 @@ BASEDIR="$(dirname "$0")"
 . $BASEDIR/assert.sh
 
 output="$(debug=on $BASEDIR/../src/editorconfig-check.sh ./windson AABA BBAB)"
-expected="git --git-dir=./windson/.git diff-tree --no-commit-id --name-only -r AABA BBAB | xargs -i sh -c 'cd ./windson && test -f {} && editorconfig-checker ./windson/{}'"
+expected="git --git-dir=./windson/.git diff-tree --no-commit-id --name-only -r AABA..BBAB | xargs -i sh -c 'cd ./windson && test -f {} && editorconfig-checker ./windson/{}'"
 assertEquals "$output" "$expected"
 
 output="$(debug=on $BASEDIR/../src/editorconfig-check.sh ./windson A)"
