42d5e1aad3182ed9aaf975cc5f336b15517781f0
Steimntz M. de Figueiredo <steimntz@tjro.jus.br>
2019-02-05 14:08:12 +0000

    ignore merge on check

--- src/editorconfig-check.sh
+++ src/editorconfig-check.sh
@@ -6,7 +6,7 @@ DIR=$1
 BEFORE_SHA=$2
 CURRENT_SHA=$3
 
-command="git --git-dir=$DIR/.git diff-tree --no-commit-id --name-only -r $BEFORE_SHA $CURRENT_SHA | xargs -i sh -c 'cd $DIR && test -f {} && editorconfig-checker $DIR/{}'"
+command="git --git-dir=$DIR/.git diff-tree --no-commit-id --name-only -r $BEFORE_SHA..$CURRENT_SHA | xargs -i sh -c 'cd $DIR && test -f {} && editorconfig-checker $DIR/{}'"
 
 if [[ "$#" -ne 3 || "$BEFORE_SHA" == "0000000000000000000000000000000000000000" ]]; then
   echo "Usage: $BASEDIR/$BASENAME DIR BEFORE_COMMIT CURRENT_COMMIT"
